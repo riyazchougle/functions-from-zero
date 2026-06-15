f5a0c8ac5b522dc2ace60caf1962dc9a6c3f86dd
riyazchougle <riyazedu786@gmail.com>
2026-06-15 14:38:59 +0000

    adding fire example

--- mylib/bot.py
+++ mylib/bot.py
@@ -1,5 +1,11 @@
 import wikipedia
 
+# Patch the old wikipedia package so it uses HTTPS + a real User-Agent
+wikipedia.wikipedia.API_URL = "https://en.wikipedia.org/w/api.php"
+wikipedia.wikipedia.USER_AGENT = (
+    "functions-from-zero-course/0.1 "
+    "(https://github.com/riyazchougle/functions-from-zero; riyazedu786@gmail.com)"
+)
 def scrape(name="Microsoft", length=1):
     result = wikipedia.summary(name, sentences=length)
     return result
